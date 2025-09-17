649e9ce295240ba50b781fee02b24ac87b49545a
wbelhome <william.belhome99@gmail.com>
2025-09-16 21:20:23 -0300

    Boas práticas

--- js/exercicios/arrays/ex2.js
+++ js/exercicios/arrays/ex2.js
@@ -1,10 +1,14 @@
 // 2) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Par encontrado.
 
-const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for (let i = 0; i < num.length; i++) {
-    const n = num[i];
-    if(n % 2 === 0){
-        console.log(n)    
-    }      
-}
+
+
+// for (let i = 0; i < num.length; i++) {
+//     const n = num[i];
+//     if(n % 2 === 0){
+//         console.log(n)    
+//     }      
+// }
+
+
